3a4ffbd1aa6db75669a6dc63abeb5cfa775705d0
rogeriodelphi <rogeriodelphi@gmail.com>
2023-01-12 02:14:15 -0300

    Criando o login e salvando em banco

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        print(nome, email, senha, confirmar_senha)
+
+        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(
+                confirmar_senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos.')
+            return render(request, 'cadastro.html')
+
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Digite duas senhas iguais.')
+            return render(request, 'cadastro.html')
+
+        try:
+            user = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha,
+            )
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
+            return render(request, 'cadastro.html')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema.')
+            return render(request, 'cadastro.html')
+
