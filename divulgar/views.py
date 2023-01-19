b0d51b9772f6bebcd6d999d43b18675a36d3aa60
rogeriodelphi <rogeriodelphi@gmail.com>
2023-01-19 01:42:17 -0300

    Criando a solicitação para adoção

--- divulgar/views.py
+++ divulgar/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Pet, Tag, Raca
 from django.contrib import messages
 from django.contrib.messages import constants
+from adotar.models import PedidoAdocao
 
 
 @login_required
@@ -41,10 +42,6 @@ def novo_pet(request):
             pet.tags.add(tag)
 
         pet.save()
-        # tags = Tag.objects.all()
-        # racas = Raca.objects.all()
-        # messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
-        # return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
         return redirect('/divulgar/seus_pets')
 
 @login_required
@@ -53,7 +50,7 @@ def seus_pets(request):
         pets = Pet.objects.filter(usuario=request.user)
         return render(request, 'seus_pets.html', {'pets':pets})
 
-
+@login_required
 def remover_pet(request, id):
     pet = Pet.objects.get(id=id)
     if not pet.usuario == request.user:
@@ -63,7 +60,14 @@ def remover_pet(request, id):
     messages.add_message(request, constants.SUCCESS, 'Pet removido com sucesso!')
     return redirect('/divulgar/seus_pets')
 
+login_required
 def ver_pet(request, id):
     if request.method == "GET":
         pet = Pet.objects.get(id=id)
         return render(request, 'ver_pet.html', {'pet': pet})
+
+login_required
+def ver_pedido_adocao(request):
+    if request.method == "GET":
+        pedidos = PedidoAdocao.objects.filter(usuario=request.user).filter(status="AG")
+        return render(request, 'ver_pedido_adocao.html', {'pedidos': pedidos})
