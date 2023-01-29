2cb1a4c36a0f0e1bfc6567a09b4973d251bfeaf0
rogeriodelphi <rogeriodelphi@gmail.com>
2023-01-29 02:52:47 -0300

    Atualizando arquivo de configuração

--- adote/settings.py
+++ adote/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-j^5dv9l&b!^u9z65nd+4r#f#z25rwy5hicxe_l8)=9!q+2)@#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
